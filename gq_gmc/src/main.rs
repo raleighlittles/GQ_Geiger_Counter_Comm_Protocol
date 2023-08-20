c6e0c0a7926351e8f04902254fda86592bf5c426
Raleigh Littles III <raleighlittles@gmail.com>
2023-08-19 23:05:12 -0700

    sendiing command works

--- gq_gmc/src/main.rs
+++ gq_gmc/src/main.rs
@@ -1,3 +1,5 @@
+use gq_gmc_protocol::send_msg;
+
 
 mod gq_gmc_protocol;
 
@@ -5,5 +7,9 @@ fn main() {
     let serial_port_file : String = std::env::args().nth(1).expect("No serial port file provided");
     let baud_rate : String = std::env::args().nth(2).expect("No baud rate provided. Please check your device settings (Others -> Comport Baud Rate");
 
-    gq_gmc_protocol::connect_to_device(serial_port_file, baud_rate.parse::<u32>().unwrap()).open().expect("Unable to open serial port!");
+    let mut serial_port = gq_gmc_protocol::connect_to_device(serial_port_file, baud_rate.parse::<u32>().unwrap()).open().expect("Unable to open serial port!");
+
+    //send_msg(serial_port, "GETVER");
+    //send_msg(&*serial_port, "GETVER".to_string()); // expected &mut dyn SerialPort, found &dyn SerialPort
+    send_msg(&mut *serial_port, "KEY3".to_string()).unwrap();
 }
