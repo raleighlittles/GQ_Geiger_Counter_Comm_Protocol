8a14ec4a717f89684307d79a0e664941be00d11e
Raleigh Littles III <raleighlittles@gmail.com>
2023-12-04 20:00:56 -0800

    more testing of commands, seems to work
    now just need to add decoding of commands

--- gq_gmc/src/commands.rs
+++ gq_gmc/src/commands.rs
@@ -1,17 +1,94 @@
 
 
 #[derive(Debug)]
-pub enum Commands {
+pub enum ParameterlessCommand {
 
     GETVER = 1,
 
     GETCPM = 2,
 
-    GETVOLT = 5
+    HEARTBEAT1 = 3,
+
+    HEARTBEAT2 = 4,
+
+    GETVOLT = 5,
+
+    GETCFG = 7,
+
+    ECFG = 8,
+
+    GETSERIAL = 11,
+
+    POWEROFF = 12,
+
+    CFGUPDATE = 13,
+
+    FACTORYRESET = 20,
+
+    REBOOT  = 21,
+
+    GETDATETIME = 23,
+
+    GETTEMP = 24,
+
+    GETGYRO = 25,
+
+    POWERON = 26,
+
+    WiFiON = 34,
+
+    WiFiOFF = 35,
+
+    WiFiLevel = 37,
+
+    EchoON = 38,
+
+    EchoOFF = 39,
+
+    ALARM1 = 40,
+
+    ALARM0 = 41,
+
+    SPEAKER1 = 42,
+
+    SPEAKER0 = 43,
+
+    GETCPS = 44,
+
+    GETMAXCPS = 45,
+
+    GETCPMH = 46,
+
+    GETCPML = 47
+
 }
 
 /// https://stackoverflow.com/questions/32710187/how-do-i-get-an-enum-as-a-string
-impl std::fmt::Display for Commands {
+impl std::fmt::Display for ParameterlessCommand {
+    
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+pub enum ButtonKeyCommand {
+
+    /// Back button
+    KEY0 = 0,
+
+    /// Down arrow
+    KEY1 = 1,
+
+    /// Up arrow
+    KEY2 = 2,
+
+    /// Power button 
+    KEY3 = 3
+    
+}
+
+impl std::fmt::Display for ButtonKeyCommand {
     
     fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(formatter, "{:?}", self)
--- gq_gmc/src/main.rs
+++ gq_gmc/src/main.rs
@@ -1,7 +1,7 @@
 use gq_gmc_protocol::send_msg;
 
-
 mod gq_gmc_protocol;
+mod commands;
 
 fn main() {
     let serial_port_file : String = std::env::args().nth(1).expect("No serial port file provided");
@@ -9,9 +9,12 @@ fn main() {
 
     let mut serial_port = gq_gmc_protocol::connect_to_device(serial_port_file, baud_rate.parse::<u32>().unwrap()).open().expect("Unable to open serial port!");
 
-    send_msg(&mut *serial_port, "GETVER".to_string()).unwrap();
+    send_msg(&mut *serial_port,  commands::ParameterlessCommand::GETGYRO.to_string()).unwrap();
     
     let mut serial_buf: Vec<u8> = vec![0; 32];
     serial_port.read(serial_buf.as_mut_slice()).expect("Found no data!");
     println!("Returned {:?}", serial_buf);
+
+    let response = std::str::from_utf8(&serial_buf).expect("invalid utf-8 sequence");
+    println!("{}", response);
 }
